b53bb37b10c17246cbdde5a34e3ffc3c019e231b
Skand <skand443@gmail.com>
2026-09-08 22:30:41 +0530

    add strings

---
+++ strings/problemA.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char characters;
+    cin>>characters;
+    int asciiChar = int(characters);
+    cout<<asciiChar<<endl;
+}
---
+++ strings/problemB.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int asciiVal;
+    cin>>asciiVal;
+    char characters = char(asciiVal);
+    cout<<characters<<endl;
+}
