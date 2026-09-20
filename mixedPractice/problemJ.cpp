23570b81ae5f1458be944e8c0db75dbadf1c2e51
Skand <skand443@gmail.com>
2026-09-20 22:48:38 +0530

    add problemI&J mixedPractice

---
+++ mixedPractice/problemJ.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    int a[10][10];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    int top = 0;
+    int bottom = m - 1;
+    int left = 0;
+    int right = n - 1;
+
+    while (top <= bottom && left <= right) {
+
+        for (int j = left; j <= right; j++) {
+            cout << a[top][j] << " ";
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            cout << a[i][right] << " ";
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                cout << a[bottom][j] << " ";
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                cout << a[i][left] << " ";
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
+
